b8e101e1d4dfcbfd92cbf7191172e555b978ffe1
unknown <11309921+lvjiaxuan@users.noreply.github.com>
2023-06-14 22:49:47 +0800

    fix: typo

--- scripts/buildDeploy.mjs
+++ scripts/buildDeploy.mjs
@@ -3,8 +3,8 @@ import fsp from 'node:fs/promises'
 
 await Promise.all([
   $`nr -C=playgrounds/vue3 build`,
-  $`nr -C=playgrounds/vue2.7 build`,
+  $`nr -C=playgrounds/vue2 build`,
 ])
 
 await fsp.cp('./playgrounds/vue3/dist', './dist', { recursive: true })
-await fsp.cp('./playgrounds/vue2.7/dist', './dist/vue2', { recursive: true })
+await fsp.cp('./playgrounds/vue2/dist', './dist/vue2', { recursive: true })
